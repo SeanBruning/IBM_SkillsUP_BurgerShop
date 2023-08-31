2770b9caadf575dd1cc342e9221c50541823ed76
Sean Bruning <134607295+SeanBruning@users.noreply.github.com>
2023-08-31 11:24:56 +0100

    Update MyOrders.jsx

--- MyOrders.jsx
+++ MyOrders.jsx
@@ -3,31 +3,43 @@ import { Link } from "react-router-dom";
 import { AiOutlineEye } from "react-icons/ai";
 
 const MyOrders = () => {
-  const arr = [1, 2, 3, 4];
+	const arr = [1, 2, 3, 4];
 
-  return (
-    <section className="tableClass">
-      <main>
-        <table>
-          <thead>
-            <tr>
-              <th>Order Id</th>
-              <th>Status</th>
-              <th>Item Qty</th>
-              <th>Amount</th>
-              <th>Payment Method</th>
-              <th>Action</th>
-            </tr>
-          </thead>
+	return (
+		<section className="tableClass">
+			<main>
+				<table>
+					<thead>
+						<tr>
+							<th>Order Id</th>
+							<th>Status</th>
+							<th>Item Qty</th>
+							<th>Amount</th>
+							<th>Payment Method</th>
+							<th>Action</th>
+						</tr>
+					</thead>
 
-          <tbody>
-//     Add the code for the table body
-           
-          </tbody>
-        </table>
-      </main>
-    </section>
-  );
+					<tbody>
+						{arr.map((order) => (
+							<tr key={order}>
+								<td>ORD{order}</td>
+								<td>Completed</td>
+								<td>3</td>
+								<td>₹1500</td>
+								<td>Card</td>
+								<td>
+									<Link to={`/order/${order}`}>
+										<AiOutlineEye />
+									</Link>
+								</td>
+							</tr>
+						))}
+					</tbody>
+				</table>
+			</main>
+		</section>
+	);
 };
 
 export default MyOrders;
